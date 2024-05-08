3eed8b05300693ac770eca6ee101b35ff8a772f2
Samuel <sjimenezp1@upao.edu.pe>
2024-05-08 13:33:47 -0500

    Se agrego la funcion cotizarEncomienda

--- EncoGestion/src/main/java/com/KelvinGarcia/EncoGestion/MAPPER/EncomiendaMapper.java
+++ EncoGestion/src/main/java/com/KelvinGarcia/EncoGestion/MAPPER/EncomiendaMapper.java
@@ -1,5 +1,32 @@
 package com.KelvinGarcia.EncoGestion.MAPPER;
 
+import com.KelvinGarcia.EncoGestion.MODEL.DTO.EncomiendaRequestDTO;
+import com.KelvinGarcia.EncoGestion.MODEL.DTO.EncomiendaResponseDTO;
+import com.KelvinGarcia.EncoGestion.MODEL.ENTITY.Encomienda;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
 public class EncomiendaMapper {
 
+    private final ModelMapper modelMapper;
+
+    public Encomienda convertToEntity(EncomiendaRequestDTO encomiendaRequestDTO) {
+        return modelMapper.map(encomiendaRequestDTO, Encomienda.class);
+    }
+
+    public EncomiendaResponseDTO convertToDTO(Encomienda encomienda) {
+        return modelMapper.map(encomienda, EncomiendaResponseDTO.class);
+    }
+
+    public List<EncomiendaResponseDTO> convertToListDTO(List<Encomienda> encomiendas) {
+        return encomiendas.stream()
+                .map(this::convertToDTO)
+                .collect(Collectors.toList());
+    }
 }
