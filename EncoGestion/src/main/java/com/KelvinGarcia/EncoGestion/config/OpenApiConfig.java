b33cdbe0e5ee7fcd67ad448dc55da4ee1dfe0f28
Kelvin Garcia <kelvingarcialopez27@gmail.com>
2024-06-20 01:40:12 -0500

    Implementacion de la documentacion con Swagger UI

--- EncoGestion/src/main/java/com/KelvinGarcia/EncoGestion/config/OpenApiConfig.java
+++ EncoGestion/src/main/java/com/KelvinGarcia/EncoGestion/config/OpenApiConfig.java
@@ -1,6 +1,47 @@
 package com.KelvinGarcia.EncoGestion.config;
 
-import lombok.Value;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.List;
+
+
+@Configuration
 public class OpenApiConfig {
+
+    //http://localhost:8080/api/v1/swagger-ui/index.html
+
+    @Value("${http://localhost:8080/api/v1}")
+    private String devUrl;
+
+    @Bean
+    public OpenAPI myOpenAPI(){
+
+        Server devServer = new Server();
+        devServer.setUrl(devUrl);
+        devServer.setDescription("Server URL in Development environment");
+
+        Contact contact = new Contact();
+        contact.setEmail("kelvingarcialopez27@gmail.com");
+        contact.setName("Kelvin");
+        contact.setUrl("http://www.garcia.com");
+
+        License mitLicense = new License().name("MIT License").url("http://choosealicense.com/license/mit/");
+
+        Info info = new Info()
+                .title("EncoGestion")
+                .version("1.0")
+                .contact(contact)
+                .description("This API exposes endpoints")
+                .termsOfService("https://encogestion.github.io/")
+                .license(mitLicense);
+
+        return new OpenAPI().info(info).servers(List.of(devServer));
+    }
 }
